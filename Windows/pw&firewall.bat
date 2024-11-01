4594f1c022bd9141ea31fa2d239a98a950374970
person613 <henrylhye@gmail.com>
2024-11-01 17:50:54 -0400

    Update pw&firewall.bat with file finding and SECPOL Settings

--- Windows/pw&firewall.bat
+++ Windows/pw&firewall.bat
@@ -15,4 +15,131 @@ netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new e
 netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no 
 netsh advfirewall firewall set rule name="Telnet Server" new enable=no 
 netsh advfirewall firewall set rule name="netcat" new enable=no
+
+echo Finding media files
+findstr .mp3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp3 > media
+findstr .ac3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ac3 >> media
+findstr .aac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aac >> media
+findstr .aiff users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aiff >> media
+findstr .flac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.flac >> media
+findstr .m4a users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4a >> media
+findstr .m4p users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4p >> media
+findstr .midi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.midi >> media
+findstr .mp2 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp2 >> media
+findstr .m3u users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m3u >> media
+findstr .ogg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ogg >> media
+findstr .vqf users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.vqf >> media
+findstr .wav users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wav >> media
+findstr .wma users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wma >> media
+findstr .mp4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp4 >> media
+findstr .avi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.avi >> media
+findstr .mpeg4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .mpeg4 >> media
+findstr .gif users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.gif >> media
+findstr .png users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.png >> media
+findstr .bmp users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.bmp >> media
+findstr .jpg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpg >> media
+findstr .jpeg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpeg >> media
+C:\WINDOWS\system32\notepad.exe media
+
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /t
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d /1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f 
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+REM Common Policies
+REM Restrict CD ROM drive
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+REM Automatic Admin logon
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+REM Logo message text
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeText /t REG_SZ /d "E"
+REM Logon message title bar
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeCaption /t REG_SZ /d "EEE"
+REM Wipe page file from shutdown
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+REM Disallow remote access to floppie disks
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+REM Prevent print driver installs 
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+REM Limit local account use of blank passwords to console
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+REM Auditing access of Global System Objects
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+REM Auditing Backup and Restore
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+REM Do not display last user on logon
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+REM UAC setting (Prompt on Secure Desktop)
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+REM Enable Installer Detection
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+REM Undock without logon
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+REM Maximum Machine Password Age
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+REM Disable machine account password changes
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+REM Require Strong Session Key
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+REM Require Sign/Seal
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+REM Sign Channel
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+REM Seal Channel
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+REM Require CTRL+ALT+DEL
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+REM Restrict Anonymous Enumeration #1
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+REM Restrict Anonymous Enumeration #2
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+REM Idle Time Limit - 45 mins
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+REM Require Security Signature - Disabled pursuant to checklist
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+REM Enable Security Signature - Disabled pursuant to checklist
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+REM Disable Domain Credential Storage
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+REM Don't Give Anons Everyone Permissions
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+REM SMB Passwords unencrypted to third party
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+REM Null Session Pipes Cleared
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+REM Remotely accessible registry paths cleared
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+REM Remotely accessible registry paths and sub-paths cleared
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+REM Restict anonymous access to named pipes and shares
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+REM Allow to use Machine ID for NTLM
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
 goto:EOF
