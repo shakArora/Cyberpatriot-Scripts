5b6f88ef3801de02af6ccff4c9ec1dc2e563c24c
developerC76 <jsb78241@gmail.com>
2024-11-12 07:12:57 -0500

    Create Antivirus.sh

---
+++ Linux/Antivirus.sh
@@ -0,0 +1,21 @@
+cd /
+#clam antivirus 
+sudo apt update && sudo apt upgrade
+sudo apt install clamav clamtk
+sudo freshclam
+sudo clamscan
+#root kit hunter
+sudo apt update && sudo apt upgrade
+sudo apt install rkhunter
+sudo rkhunter --update
+sudo rkhunter --check
+sudo rkhunter --check --skipped
+sudo less /var/log/rkhunter.log
+sudo crontab -e
+#rkhunter scan every day at 18:00
+1 8 * * * /usr/bin/rkhunter --check --cronjob
+#chrootkit
+sudo apt update && sudo apt upgrade
+sudo apt install chrootkit
+sudo apt upgrade chrootkit
+sudo chrootkit
