6b326ca45b453a618d300b2a18ad25a9be3c797e
developerC76 <jsb78241@gmail.com>
2024-11-12 18:07:20 -0500

    Update filefinding.sh
    Added sudo apt update and upgrade to the code so that happens while it’s running

--- Linux/filefinding.sh
+++ Linux/filefinding.sh
@@ -1,3 +1,4 @@
+sudo apt update && sudo apt upgrade
 find . -name '*.mp3' -type f -print
 find . -name '*.mov' -type f -print
 find . -name '*.mp4' -type f -print
@@ -9,6 +10,7 @@ find . -name '*.m4a' -type f -print
 find . -name '*.flv' -type f -print
 find . -name '*.ogg' -type f -print
 find . -name '*.mov' -type f -print
+sudo apt update && sudo apt upgrade
 find /home -name '*.gif' -type f -print
 find /home -name '*.png' -type f -print
 find /home -name '*.jpg' -type f -print
@@ -17,6 +19,7 @@ find /home -name '*.tar.gz' -type f -print
 find /home -name '*.appImage' -type f -print
 find /home -name '*.exe' -type f -print
 find /home -name '*.html' -type f -print
+sudo apt update && sudo apt upgrade
 find /home -name '*.js' -type f -print
 find /home -name '*.jpeg' -type f -print
 find /home -name '*.cpp' -type f -print
