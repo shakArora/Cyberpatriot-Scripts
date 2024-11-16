0351458ff7219a72cb6d198ebaa2785443d0c5f0
Shivum Arora <126220391+shakArora@users.noreply.github.com>
2024-11-15 22:48:49 -0500

    Update Checkinit.sh

--- Linux/Checkinit.sh
+++ Linux/Checkinit.sh
@@ -2,6 +2,6 @@ cd /
 sudo --version
 apt --version
 echo $PATH
-sudo apt update && sudo apt upgrade
-sudo apt-get update && sudo apt-get upgrade
+# sudo apt update && sudo apt upgrade
+# sudo apt-get update && sudo apt-get upgrade
 sudo apt install neofetch && neofetch
