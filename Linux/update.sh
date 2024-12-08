12ee7a5b58c7c6b57de2ff44737c194d76d49963
jimjam <jsb78241@gmail.com>
2024-12-08 16:40:13 -0500

    Create update.sh

---
+++ Linux/update.sh
@@ -0,0 +1,13 @@
+sudo apt update && sudo apt full-upgrade && sudo apt dist-upgrade
+apt upgrade -a --upgradable
+echo "Basic apt updates complete"
+sudo snap refresh
+echo "Snap updates completed"
+echo "For version updates go to the 'software updater' and 'software and updates' apps by pressing the super and a keys. Then update all packages."
+echo "Should I open Software Updater? (1 or 0)" 
+read -p "To open it type 1 else type 0: " $open
+if ["$open" == "1"]; then 
+  update-manager
+else
+  echo "Exiting process"
+fi
