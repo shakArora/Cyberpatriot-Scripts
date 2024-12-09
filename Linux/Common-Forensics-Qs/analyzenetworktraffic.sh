968fe35c69a329ba400a7bbbf5e72b88482e142a
person613 <henrylhye@gmail.com>
2024-12-09 09:17:09 -0500

    Update analyzenetworktraffic.sh

--- Linux/Common-Forensics-Qs/analyzenetworktraffic.sh
+++ Linux/Common-Forensics-Qs/analyzenetworktraffic.sh
@@ -5,3 +5,4 @@ tcpdump -i eth0 -w capture.pcap -n -t -s 0 -W 1 -C 100
 
 # Analyze the captured packet with Wireshark
 wireshark capture.pcap
+# You can only use this part if wireshark is installed on the computer, dont install if you aren't told to
