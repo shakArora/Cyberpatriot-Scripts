366f73c58d220770b79227d123dffae1bc327509
jimjam <jsb78241@gmail.com>
2024-12-12 09:11:26 -0500

    Update makeAllScriptsExecutable.sh
    added all scripts in /linux/main

--- Linux/makeAllScriptsExecutable.sh
+++ Linux/makeAllScriptsExecutable.sh
@@ -5,9 +5,18 @@ chmod +x incidentresponse.sh
 chmod +x inodeanalysis.sh
 chmod +x malwareanalysis.sh
 chmod +x networkforensics.sh
+
 chmod +x Checkinit.sh
 chmod +x Installsudoandapt.sh
 chmod +x antivirus_optimization_ufw_passwords_ipv6.sh
 chmod +x checkAllPorts.sh
 chmod +x filefinding.sh
 chmod +x gemini.sh
+
+chmod +x Antivirus.sh
+chmod +x Users.sh
+chmod +x checkapplications.sh
+chmod +x lynis.sh
+chmod +x ports_and_ufw.sh
+chmod +x systemhardening1.sh
+chmod +x update.sh
