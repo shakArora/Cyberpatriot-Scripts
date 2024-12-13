7e2f1bc31f3adcda14d7f4a9d379eac1b062e3fd
Shivum Arora <126220391+shakArora@users.noreply.github.com>
2024-12-13 18:44:26 -0500

    Update Antivirus.sh

--- Linux/main/Antivirus.sh
+++ Linux/main/Antivirus.sh
@@ -3,41 +3,32 @@
 echo "Welcome to the block1 Linux Antivirus Procedure"
 
 cd /
-echo "Would you like to run ClamAV (1/0)"
-read -p " " $clam
-if ["$clam" == "1"]; then 
-  sudo apt install clamav clamtk
-  sudo freshclam
-  sudo clamscan -r /
-fi
+sudo apt install clamav clamtk
+sudo freshclam
+sudo clamscan -r /
 
-echo "Would you like to run RKHunter and ChRootKit (1/0)"
-read -p " " $rootkit
-if ["$rootkit" == "1"]; then
-  sudo apt update && sudo apt install rkhunter
-  sudo rkhunter --update
-  sudo rkhunter --check
-  sudo rkhunter --check --skipped
-  sudo less /var/log/rkhunter.log
-  sudo crontab -e
-  #rkhunter scan every day at 18:00
-  1 8 * * * /usr/bin/rkhunter --check --cronjob
-  #chrootkit
-  # sudo apt update && sudo apt upgrade
-  sudo apt install chrootkit
-  sudo apt upgrade chrootkit
-  sudo chrootkit
-fi
+sudo apt update && sudo apt install rkhunter
+sudo rkhunter --update
+sudo rkhunter --check
+sudo rkhunter --check --skipped
+sudo less /var/log/rkhunter.log
+sudo crontab -e
+#rkhunter scan every day at 18:00
+1 8 * * * /usr/bin/rkhunter --check --cronjob
+#chrootkit
+# sudo apt update && sudo apt upgrade
+sudo apt install chrootkit
+sudo apt upgrade chrootkit
+sudo chrootkit
+
 
 sudo apt update && sudo apt upgrade -y
 echo "Updating the system and installing necessary packages"
 sudo apt install lynis clamav nmap fail2ban ufw
 
-echo "Would you like to run Lynis (1/0): "
-read -p " " $lynissoftware
-if ["$lynissoftware" == "1"]; then
-  sudo lynis audit system --quick --detailed > lynis_report_$(date +%Y-%m-%d_%H-%M-%S).txt
-fi
+
+sudo lynis audit system --quick --detailed > lynis_report_$(date +%Y-%m-%d_%H-%M-%S).txt
+
 
 echo "Checking for open ports"
 # Check for open ports
